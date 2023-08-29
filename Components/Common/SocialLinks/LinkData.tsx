a11535974819d027a8187f4229e6b37934463660
Jay Jasani <jasanijay8@gmail.com>
2023-08-29 14:10:43 +0530

    repo

---
+++ Components/Common/SocialLinks/LinkData.tsx
@@ -0,0 +1,45 @@
+export const homePageSocialLinks: ISocialLink[] = [
+    {
+        name: "LinkedIn",
+        svg: "LinkedIn",
+        url: "https://www.linkedin.com/in/jay-jasani-917576227",
+    },
+    {
+        name: "Dev.to",
+        svg: "DevTo",
+        url: "https://dev.to/jayjasani",
+    },
+    {
+        name: "Instagram",
+        svg: "Instagram",
+        url: "https://instagram.com/jayy.jasani?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+    },
+    {
+        name: "Facebook",
+        svg: "Facebook",
+        url: "https://www.facebook.com/jay.jasani.589?mibextid=2JQ9oc",
+    },
+    {
+        name: "GitHub",
+        svg: "GitHub",
+        url: "https://github.com/JayJasani",
+    },
+];
+
+export const codingProfilesLinks: ISocialLink[] = [
+    {
+        name: "LeetCode",
+        svg: "LeetCode",
+        url: "https://leetcode.com/omkathe26/",
+    },
+    {
+        name: "GeeksForGeeks",
+        svg: "GFG",
+        url: "https://auth.geeksforgeeks.org/user/omkathe26/practice",
+    },
+    {
+        name: "HackerRank",
+        svg: "HackerRank",
+        url: "https://www.hackerrank.com/omkathe",
+    },
+];
